a290a402c636cafd591e8b206c6c06e92c44b5bc
Cashton Harmer <63674523+CoolCash1@users.noreply.github.com>
2020-05-05 16:11:35 -0500

    Add files via upload

---
+++ alpha/R6/ardUX_Project.h
@@ -0,0 +1,32 @@
+/*
+
+   ardUX alpha 6 patch notes
+
+                |-|  |-|  \--\  /--/
+                | |  | |   \  \/  /
+           ARD  | |  | |    >    <
+                | |__| |   /  /\  \
+                 \_____/  /__/  \__\
+            ARDUINO'S PC-EQUIVLENT
+ 
+   -Improved "Boot" Speed
+   -Prepared for alpha 7's "servoMove" feature
+
+
+                      Commands
+   -animation
+    |-syntax: animation 1 or animation 0
+
+   -Version
+    |-syntax: Version
+
+   -playSound
+    |-syntax: playSound
+
+   To use commands open serial monitor, type command 
+   in text bar and press "Send".
+
+
+
+   2020 ardUX owned and maintained by Cashton Harmer
+                     Mar 1st 2020
