b8244b7c9a0c762969832accb53e77753c09b0fd
Cashton Harmer <63674523+CoolCash1@users.noreply.github.com>
2020-05-05 16:09:56 -0500

    Add files via upload

---
+++ alpha/R4/ardUX_Project.h
@@ -0,0 +1,31 @@
+/*
+
+   ardUX alpha 4 patch notes
+
+                |-|  |-|  \--\  /--/
+                | |  | |   \  \/  /
+           ARD  | |  | |    >    <
+                | |__| |   /  /\  \
+                 \_____/  /__/  \__\
+            ARDUINO'S PC-EQUIVLENT
+ 
+   -added command "Version" used to check system OS
+    version.
+
+   -fixed a problom with commands
+
+
+                      Commands
+   -animation
+    |-syntax: animation 1 or animation 0
+
+   -Version
+    |-syntax: Version
+
+   To use commands open serial monitor, type command 
+   in text bar and press "Send".
+
+
+
+   2020 ardUX owned and maintained by Cashton Harmer
+                     Feb 28th 2020
