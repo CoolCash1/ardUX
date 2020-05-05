6d727cfad62194c864bb65f070de5536b57c2c49
Cashton Harmer <63674523+CoolCash1@users.noreply.github.com>
2020-05-05 16:10:52 -0500

    Add files via upload

---
+++ alpha/R5/ardUX_Project.h
@@ -0,0 +1,32 @@
+/*
+
+   ardUX alpha 5 patch notes
+
+                |-|  |-|  \--\  /--/
+                | |  | |   \  \/  /
+           ARD  | |  | |    >    <
+                | |__| |   /  /\  \
+                 \_____/  /__/  \__\
+            ARDUINO'S PC-EQUIVLENT
+ 
+   -This one took a wihile. added command "playSound"
+   -Surpassed line 200!
+
+
+                      Commands
+   -animation
+    |-syntax: animation 1 or animation 0
+
+   -Version
+    |-syntax: Version
+
+   -playSound
+    |-syntax: playSound
+
+   To use commands open serial monitor, type command 
+   in text bar and press "Send".
+
+
+
+   2020 ardUX owned and maintained by Cashton Harmer
+                     Feb 28th 2020
