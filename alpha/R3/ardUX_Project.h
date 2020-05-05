3ab394be695cf22e5bcb060962e9b63267b3dabe
Cashton Harmer <63674523+CoolCash1@users.noreply.github.com>
2020-05-05 16:09:08 -0500

    Add files via upload

---
+++ alpha/R3/ardUX_Project.h
@@ -0,0 +1,27 @@
+/*
+
+   ardUX alpha 3 patch notes
+
+                |-|  |-|  \--\  /--/
+                | |  | |   \  \/  /
+           ARD  | |  | |    >    <
+                | |__| |   /  /\  \
+                 \_____/  /__/  \__\
+            ARDUINO'S PC-EQUIVLENT
+ 
+   -added "Commands" Section to Patch Notes
+   -Fixed some grammar. :)
+   -new variable "BIOSVersion"
+
+
+                      Commands
+   -animation
+    |-syntax: animation 1 or animation 0
+
+   To use commands open serial monitor, type command 
+   in text bar and press "Send".
+
+
+
+   2020 ardUX owned and maintained by Cashton Harmer
+                     Feb 28th 2020
