343d31f373495b342e221dca3a148ecde068df5a
Cashton Harmer <63674523+CoolCash1@users.noreply.github.com>
2020-05-05 16:08:16 -0500

    Add files via upload

---
+++ alpha/R2/ardUX_Project.h
@@ -0,0 +1,19 @@
+/*
+
+   ardUX alpha 2 patch notes
+
+                |-|  |-|  \--\  /--/
+                | |  | |   \  \/  /
+           ARD  | |  | |    >    <
+                | |__| |   /  /\  \
+                 \_____/  /__/  \__\
+            ARDUINO'S PC-EQUIVLENT
+ 
+   -changed intro song
+   -remove "SI (Serial Interface) Mode" text when Booting
+   -added patchnotes
+
+
+
+
+   2020 ardUX owned and maintained by Cashton Harmer
